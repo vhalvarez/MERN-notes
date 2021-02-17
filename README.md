2afb45ce773ef02cc9cb15984906b67cb306eb1e
vhalvarez <alvarez.programmer@gmail.com>
2021-02-17 15:52:13 -0400

    Notes pages con MERN

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-## Mi primer proyecto utilizando MERN Stack :+1:
+# Notes
+
+'Notes' es una página creada en MERN que se basa en la creacion, eliminación y actualización de notas y usuarios.
+
+## Usage
+
+```javascript
+yarn start 
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 
 //settings (configurar servidor)
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 8080);
 
 //middlewares
 app.use(cors());
@@ -12,5 +12,10 @@ app.use(express.json());
 //routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/notes', require('./routes/notes'));
+
+//heroku
+if (process.env.NODE_ENV === 'production'){
+    
+}
 
 module.exports = app;
